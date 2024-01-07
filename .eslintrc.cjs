3b06f453efa01af508e2fd0b76606836f1df672d
Jake McInerney <jakemcinerney1@gmail.com>
2024-01-07 13:53:53 -0600

    added vite, sass, and eslint rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb/whitespace",
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+    "react/react-in-jsx-scope": "off",
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/jsx-no-target-blank */
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import './App.scss';
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <div>Count is:</div>
+        <button
+          type="button"
+          onClick={() => setCount(count + 1)}
+        >
+          {count}
+        </button>
+        <p>
+          Edit
+          <code>src/App.tsx</code>
+          and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
