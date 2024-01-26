7d42319ced850b688ef697b480c5fedb4abf7813
Brandon Barnes <imbrandonbarnes@gmail.com>
2024-01-26 10:53:09 -0800

    CIS-27 Built out Footer Component - Updated Footer component to apply ESLint > Added "aria-label"s to all anchors
    - Placed footer at the bottom of App
    
    Notes :
    - Footer is built using Anchors, should we move it over to React Router Links?
    
    - Links are currently placeholders to be changes later
    
    - Need a Logo to place on the footer
    
    - Should I remove additional files from the Footer component?

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import { ExampleFeature } from "./domains/example-feature/example-feature.index";
 import { store } from "./redux/store";
+import Footer from "./lib/Footer/Footer.component";
 
 // This will be used to compose all the components from this feature
 // All routing will also live here
@@ -9,6 +10,7 @@ function App() {
   return (
     <Provider store={store}>
       <ExampleFeature />
+      <Footer />
     </Provider>
   );
 }
