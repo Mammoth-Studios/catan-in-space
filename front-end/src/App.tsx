caef5b1bbf095af0a676c72f7bcc3185f2276a7c
dustinjack99 <dustin.guy.jackson@gmail.com>
2024-01-27 14:25:34 -0600

    [CIS-0000] fix(mobile): Fixed folder structure and index imports

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,7 @@
 import { Provider } from "react-redux";
 import { ExampleFeature } from "./domains/example-feature/example-feature.index";
 import { store } from "./redux/store";
-import { Footer } from "./lib/Footer/Footer.index";
+import { Footer } from "@lib/Footer";
 
 // This will be used to compose all the components from this feature
 // All routing will also live here
--- front-end/src/lib/Footer/Footer.component.tsx
+++ front-end/src/lib/Footer/Footer.component.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Footer.style.scss";
 
-import {FacebookLogo, InstagramLogo, XLogo} from "../Social-Logos/Socials.index";
+import { FacebookLogo, InstagramLogo, XLogo } from "@lib/Social-Logos";
 
 type LinkData = {
-  href: string,
-  label: string,
-  text: string
+  href: string;
+  label: string;
+  text: string;
 };
 
 // @ts-expect-error TS2304: Cannot find name 'Array'.
@@ -14,9 +14,17 @@ const footerLinks: Array<LinkData> = [
   { href: "/home", label: "Home Page", text: "Home" },
   { href: "/contact", label: "Contact Page", text: "Contact" },
   { href: "/how-to-play", label: "How To Play Page", text: "How To Play" },
-  { href: "/policy/privacy", label: "Privacy Policy Page", text: "Privacy Policy" },
-  { href: "/policy/TOS", label: "Terms Of Service Page", text: "Terms of Service" },
-  { href: "/careers", label: "Careers Page", text: "Careers" }
+  {
+    href: "/policy/privacy",
+    label: "Privacy Policy Page",
+    text: "Privacy Policy",
+  },
+  {
+    href: "/policy/TOS",
+    label: "Terms Of Service Page",
+    text: "Terms of Service",
+  },
+  { href: "/careers", label: "Careers Page", text: "Careers" },
 ];
 
 export const Footer: React.FC = () => (
@@ -24,13 +32,15 @@ export const Footer: React.FC = () => (
     <img src="./PLACEHOLDER" alt="Catan In Space Logo" />
     <div className="links">
       {footerLinks.map((link: LinkData) => (
-        <a key={link.href} href={link.href} aria-label={link.label}>{link.text}</a>
+        <a key={link.href} href={link.href} aria-label={link.label}>
+          {link.text}
+        </a>
       ))}
     </div>
     <div className="socials">
-      <FacebookLogo/>
-      <InstagramLogo/>
-      <XLogo/>
+      <FacebookLogo />
+      <InstagramLogo />
+      <XLogo />
     </div>
     <p>© 2024 Mammoth Studios, All rights reserved.</p>
   </footer>
