bf500ed1b70a918432e1a83f8047e81e88f489d1
404pandas <113853550+404pandas@users.noreply.github.com>
2024-02-03 09:18:32 -0500

    Fixed mammoth image import path on /game-board/game-placeholder/game-placeholder.component.tsx

--- front-end/src/domains/game-board/components/game-placeholder/game-placeholder.component.tsx
+++ front-end/src/domains/game-board/components/game-placeholder/game-placeholder.component.tsx
@@ -1,23 +1,23 @@
-import mammoth from "@views/game-view/assets/mamoth-house.png";
+import mammoth from "@views/assets/mamoth-house.png";
 
 // This will be used to compose all the components from this feature
 export const GamePlaceHolder = () => (
   <>
-    <div className="flexbox-turn-on placeholder-container">
-      <div className="flip-container">
+    <div className='flexbox-turn-on placeholder-container'>
+      <div className='flip-container'>
         <h2>Game Page</h2>
 
-        <div className="flipper">
-          <div className="front flexbox-turn-on">
-            <div className="flexbox-turn-on placeholder-content">
-              <img src={mammoth} className="placeholder-img"></img>
+        <div className='flipper'>
+          <div className='front flexbox-turn-on'>
+            <div className='flexbox-turn-on placeholder-content'>
+              <img src={mammoth} className='placeholder-img'></img>
               <h2>In Development!</h2>
               <h2>Stay tuned!</h2>
             </div>
           </div>
-          <div className="back flexbox-turn-on">
-            <div className="flexbox-turn-on placeholder-content">
-              <img src={mammoth} className="placeholder-img"></img>
+          <div className='back flexbox-turn-on'>
+            <div className='flexbox-turn-on placeholder-content'>
+              <img src={mammoth} className='placeholder-img'></img>
               <h2>In Development!</h2>
               <h2>Stay tuned!</h2>
             </div>
