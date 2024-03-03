2615c8cb315469da4e0222ee358011772731be68
dustinjack99 <dustin.guy.jackson@gmail.com>
2024-03-02 19:41:09 -0600

    updated footer with new component, added credits section

--- front-end/src/domains/home/components/home-about/home-about.component.tsx
+++ front-end/src/domains/home/components/home-about/home-about.component.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import "./home-about.style.scss";
-import { aboutPanels } from "./home-about.constant";
+import { aboutPanels, credits } from "./home-about.constant";
 import AboutPanel from "../home-about-panel/home-about-panel.component";
 
 const HomeAbout: FC = () => {
@@ -15,6 +15,23 @@ const HomeAbout: FC = () => {
           position={index % 2 ? "left" : "right"}
         />
       ))}
+      <div className="home-about-credits">
+        <h2>Credits</h2>
+        <div className="home-about-credits-container">
+          {credits.map(({ department, roles }, index) => (
+            <div key={index} className="home-about-credits-department">
+              <h3>{department}</h3>
+              <ul>
+                {roles.map(({ name, role }, index) => (
+                  <li key={index}>
+                    {name} - {role}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
