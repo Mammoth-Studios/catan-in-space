2a6106799f3fe082c126c559c10d7f749c4aa338
Val-code-1 <reit.ben@gmail.com>
2024-03-07 16:49:35 -0600

    First draft of CIS-49 Hero Section

---
+++ front-end/src/domains/home/components/home-hero-section/hero-section.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import "./hero-section.scss";
+import placeholderimage from "./placeholder-assets/user-image.jpg";
+
+export const HeroSection: FC = () => (
+  <section className="hero-section">
+    <div className="hero-section-image-div"></div>
+    <img className="hero-section-image" src={placeholderimage}></img>
+    <p className="hero-section-image-focus">Image focus</p>
+    <div className="hero-section-div">
+      <p className="hero-section-title">
+        Play online in browser <br />
+        (or tagline goes here!)
+      </p>
+      <Link to="/game">
+        <button className="hero-section-button">PLAY NOW</button>
+      </Link>
+      <a className="hero-section-signin" href="">
+        Or Sign in
+      </a>
+    </div>
+  </section>
+);
