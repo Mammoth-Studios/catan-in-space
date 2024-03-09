fb6381e8b5169071ddef828b160074e33d9804f6
Val-code-1 <reit.ben@gmail.com>
2024-03-08 18:23:30 -0600

    Finished first prototype of hero section

--- front-end/src/domains/home/components/home-hero-section/hero-section.tsx
+++ front-end/src/domains/home/components/home-hero-section/hero-section.tsx
@@ -5,9 +5,10 @@ import placeholderimage from "./placeholder-assets/user-image.jpg";
 
 export const HeroSection: FC = () => (
   <section className="hero-section">
-    <div className="hero-section-image-div"></div>
-    <img className="hero-section-image" src={placeholderimage}></img>
-    <p className="hero-section-image-focus">Image focus</p>
+    <div className="hero-section-image-div">
+      <img className="hero-section-image" src={placeholderimage}></img>
+      <p className="hero-section-image-focus">Image focus</p>
+    </div>
     <div className="hero-section-div">
       <p className="hero-section-title">
         Play online in browser <br />
