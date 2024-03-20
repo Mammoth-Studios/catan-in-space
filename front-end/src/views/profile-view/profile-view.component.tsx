afea641d58eff6f75d8b85296a780c75756d5e29
Samsonly <adam.samson.perry@gmail.com>
2024-03-20 14:54:11 -0500

    [CIS-66] Edit Game Settings Button/Modal

--- front-end/src/views/profile-view/profile-view.component.tsx
+++ front-end/src/views/profile-view/profile-view.component.tsx
@@ -2,10 +2,11 @@ import { FC } from "react";
 import { FriendsList } from "@domains/friends/components/friends-list";
 import { FriendsRequests } from "@domains/friends/components/friends-requests";
 import { FriendsSearch } from "@domains/friends/components/friends-search";
+import { FriendsEditSettingsButton } from "@domains/friends/components/friends-edit-settings-button";
 
 export const ProfileView: FC = () => (
   <>
-    {"what's up samson!"}
+    <FriendsEditSettingsButton />
     <FriendsList />
     <FriendsRequests />
     <FriendsSearch />
