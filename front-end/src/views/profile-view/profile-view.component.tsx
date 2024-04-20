cf3c046a1b8b5166b16f3e4715aeb1319195ffc0
dustinjack99 <dustin.guy.jackson@gmail.com>
2024-04-20 09:28:08 -0500

    cleaned up some CSS and reconciled button components

--- front-end/src/views/profile-view/profile-view.component.tsx
+++ front-end/src/views/profile-view/profile-view.component.tsx
@@ -2,11 +2,11 @@ import { FC } from "react";
 import { FriendsList } from "@domains/friends/components/friends-list";
 import { FriendsRequests } from "@domains/friends/components/friends-requests";
 import { FriendsSearch } from "@domains/friends/components/friends-search";
-import { FriendsEditSettingsButton } from "@domains/friends/components/friends-edit-settings-button";
+import { Profile } from "@domains/friends/components/profile";
 
 export const ProfileView: FC = () => (
   <>
-    <FriendsEditSettingsButton />
+    <Profile />
     <FriendsList />
     <FriendsRequests />
     <FriendsSearch />
